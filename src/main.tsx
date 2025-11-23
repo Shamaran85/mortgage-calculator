703ad5046f97e6e1a5c3791fbc7e8258d69897f0
Daniel <dwawired@gmail.com>
2025-11-23 19:03:33 +0100

    Fixes 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import "@fontsource/roboto/300.css?inline";
+import "@fontsource/roboto/400.css?inline";
+import "@fontsource/roboto/500.css?inline";
+import "@fontsource/roboto/700.css?inline";
 import { registerSW } from "virtual:pwa-register";
 registerSW();
 
